f0ef67149aebef1941f0ecb231a82927aa67773f
Swami Chandrasekaran <swamichandra@gmail.com>
2023-03-24 09:25:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -108,7 +108,7 @@ with st.form('app'):
 
     with col1:
         #optional_ingredient = st.text_input('I will come up with ingredients. Any particular ones you want me to add?', placeholder="Add as many ingredients you want me to use seperated by commas")
-        optional_ingredient = st.multiselect(label='I will choose the ingredients. Any specific ones to incoporate', options=ingredients,)
+        optional_ingredient = st.multiselect(label='I will decide on the ingredients. Any particular ones to include?', options=ingredients,)
         print(optional_ingredient)
 
     with col2:
