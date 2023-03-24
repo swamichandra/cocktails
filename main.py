5a5d7ec1e6bdfa5bfc9d4478b4361ae2909956ed
Swami Chandrasekaran <swamichandra@gmail.com>
2023-03-24 07:00:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -152,7 +152,7 @@ if btn:
         with col2:
             st.subheader("How will this drink look?")
             #st.markdown(drink)
-            prompt_4_diffusion = "alcoholic " + drink + " drink named " + cocktail_name + ". Normal Rockwell, Tom Bagshaw, matte, dramatic lighting --s 42000 --ar 4:3 --version 3"
+            prompt_4_diffusion = "alcoholic " + drink + " drink named " + cocktail_name + ". Normal Rockwell, Tom Bagshaw, matte, dramatic lighting --s 42000 --ar 4:3 --vibefast"
             #st.markdown(prompt_4_diffusion.strip())
             print(prompt_4_diffusion)
 
