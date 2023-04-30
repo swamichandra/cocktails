8548d95608663a59891e70c6015e64be78dae4a5
Swami Chandrasekaran <swamichandra@gmail.com>
2023-04-30 12:19:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ st.markdown("""
                .block-container {
                     padding-top: 1rem;
                     padding-bottom: 0rem;
-                    padding-left: 5rem;
-                    padding-right: 5rem;
+                    padding-left: 1rem;
+                    padding-right: 1rem;
                 }
         </style>
         """, unsafe_allow_html=True)
