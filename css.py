bf0821e44fd6d8c194c16d9f5a78b3a115cb2fc5
Swami Chandrasekaran <swamichandra@gmail.com>
2023-09-01 21:33:52 -0500

    Update css.py

--- css.py
+++ css.py
@@ -6,59 +6,29 @@ html, body, [class*="css"] {
 }
 
 div.stButton > button {
-    display: inline-block;
-    outline: 0;
-    cursor: pointer;
-    border: none;
-    padding: 0 56px;
-    height: 45px;
-    line-height: 45px;
-    border-radius: 7px;
-    background-color: #0070f3;
-    color: white;
-    font-weight: 400;
-    font-size: 16px;
-    box-shadow: 0 4px 14px 0 rgb(0 118 255 / 39%);
-    transition: background 0.2s ease,color 0.2s ease,box-shadow 0.2s ease;
-    :hover{
-        background: rgba(0,118,255,0.9);
-        box-shadow: 0 6px 20px rgb(0 118 255 / 23%);
-    }
-}     
 
-.css-1swdl48,.edgvbvh10{
-    display: inline-block;
-    outline: none;
-    cursor: pointer;
-    font-weight: 500;
-    border-radius: 3px;
-    padding: 0 16px;
-    border-radius: 4px;
-    color: #fff;
-    background: #6200ee;
-    line-height: 1.15;
-    font-size: 14px;
-    height: 36px;
-    word-spacing: 0px;
-    letter-spacing: .0892857143em;
-    text-decoration: none;
-    text-transform: uppercase;
-    min-width: 64px;
-    border: none;
-    text-align: center;
-    box-shadow: 0px 3px 1px -2px rgb(0 0 0 / 20%), 0px 2px 2px 0px rgb(0 0 0 / 14%), 0px 1px 5px 0px rgb(0 0 0 / 12%);
-    transition: box-shadow 280ms cubic-bezier(0.4, 0, 0.2, 1);
-    :hover {
-        background: rgb(98, 0, 238);
-        box-shadow: 0px 2px 4px -1px rgb(0 0 0 / 20%), 0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%);
-    }
+                    display: inline-block;
+                    outline: none;
+                    cursor: pointer;
+                    font-weight: 600;
+                    border-radius: 3px;
+                    padding: 12px 24px;
+                    border: 0;
+                    color: #fff;
+                    background: #ff5000;
+                    line-height: 1.15;
+                    font-size: 16px;
+                    :hover {
+                        transition: all .1s ease;
+                        box-shadow: 0 0 0 0 #fff, 0 0 0 3px #1de9b6;
+                    }
                 
-}
+                
+                                     
+}     
 
 .small-font {
     font-size: 9px !important;
     font-weight : bold;
 }
-
-
 """
