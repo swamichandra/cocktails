ac07d25dc2cdfc27ce603ddb84a70d3621347afd
Swami Chandrasekaran <swamichandra@gmail.com>
2023-09-01 23:53:18 -0500

    Update css.py

--- css.py
+++ css.py
@@ -7,21 +7,28 @@ html, body, [class*="css"] {
 
 div.stButton > button {
 
-                    display: inline-block;
-                    outline: none;
-                    cursor: pointer;
-                    font-weight: 600;
-                    border-radius: 3px;
-                    padding: 12px 24px;
-                    border: 0;
-                    color: #fff;
-                    background: #ff5000;
-                    line-height: 1.15;
-                    font-size: 16px;
-                    :hover {
-                        transition: all .1s ease;
-                        box-shadow: 0 0 0 0 #fff, 0 0 0 3px #1de9b6;
-                    }
+                   
+
+                display: inline-block;
+                outline: 0;
+                cursor: pointer;
+                border: none;
+                padding: 0 56px;
+                height: 45px;
+                line-height: 45px;
+                border-radius: 7px;
+                background-color: #cc4d29;
+                color: white;
+                font-weight: 400;
+                font-size: 16px;
+                box-shadow: 0 4px 14px 0 rgb(0 118 255 / 39%);
+                transition: background 0.2s ease,color 0.2s ease,box-shadow 0.2s ease;
+                :hover{
+                    background: rgba(0,118,255,0.9);
+                    box-shadow: 0 6px 20px rgb(0 118 255 / 23%);
+                }
+                
+            
                 
                 
                                      
@@ -31,4 +38,5 @@ div.stButton > button {
     font-size: 9px !important;
     font-weight : bold;
 }
+
 """
